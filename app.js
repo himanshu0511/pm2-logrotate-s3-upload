1b378b7fdccfed228c876efc5613c3fd49f720e0
Himanshu Pandey <himanshu_03@hotmail.com>
2018-09-12 11:31:16 +0530

    remove junk code from app.js

--- app.js
+++ app.js
@@ -6,35 +6,6 @@ var moment  	= require('moment-timezone');
 var scheduler	= require('node-schedule');
 var zlib      = require('zlib');
 var deepExtend = require('deep-extend');
-var https = require('https');
-
-function getCall() {
-    //initialize options values, the value of the method can be changed to POST to make https post calls
-    var options = {
-        host :  'graph.facebook.com',
-        port : 443,
-        path : '/debug_token?input_token=' + userAccessToken + '&access_token=' + appAccessToken,
-        method : 'GET'
-    }
-
-    //making the https get call
-    var getReq = https.request(options, function(res) {
-        console.log("\nstatus code: ", res.statusCode);
-        res.on('data', function(data) {
-            console.log( JSON.parse(data) );
-        });
-    });
-
-    //end the request
-    getReq.end();
-    getReq.on('error', function(err){
-        console.log("Error: ", err);
-    });
-}
-
-getCall();
-
-
 var conf = pmx.initModule({
   widget : {
     type             : 'generic',
